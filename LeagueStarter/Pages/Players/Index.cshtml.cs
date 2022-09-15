bc3a3779ea658ed6978805c739124d2aeca2c9ee
Matt B <mattbuckwell@gmail.com>
2022-09-15 15:50:37 +1200

    Added code and markup to display all players in the index page of Players folder.

--- LeagueStarter/Pages/Players/Index.cshtml.cs
+++ LeagueStarter/Pages/Players/Index.cshtml.cs
@@ -2,15 +2,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using League.Data;
+using League.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace League.Pages.Players
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly LeagueContext _context;
+
+        public IndexModel (LeagueContext context)
         {
+            _context = context;
+        }
+
+        public List<Player> Players { get; set; }
+
+        public string FavouriteTeam { get; set; }
+        public async Task OnGetAsync()
+        {
+            var players = from p in _context.Players
+                          select p;
+
+            // Store the favourite team from the cookie
+            FavouriteTeam = HttpContext.Session.GetString("_Favourite");
+            // Retrieve any players from the search, sort or filter options
+            Players = await players.ToListAsync();
+        }
+
+        public string PlayerClass(Player Player)
+        {
+            string Class = "d-flex";
+            if(Player.Depth == 1)
+                Class += " starter";
+            if (Player.TeamId == FavouriteTeam)
+                Class += " favourite";
+            return Class;
         }
     }
 }
--- LeagueStarter/Pages/Players/Player.cshtml.cs
+++ LeagueStarter/Pages/Players/Player.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using League.Data;
+using League.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,17 @@ namespace League.Pages.Players
 {
     public class PlayerModel : PageModel
     {
-        public void OnGet()
+        private readonly LeagueContext _context;
+
+        public PlayerModel (LeagueContext context)
         {
+            _context = context;
+        }
+
+        public Player Player { get; set; }
+        public async Task OnGetAsync(string id)
+        {
+            Player = await _context.Players.FindAsync(id);
         }
     }
 }
