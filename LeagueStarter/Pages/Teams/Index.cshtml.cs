edfc1b55193c9f6eb0bdea8e38aaee3ff021ca42
Matt B <mattbuckwell@gmail.com>
2022-09-15 12:29:23 +1200

    Adding code and markup to the Index page under Teams folder to display conferences, divisions and teams.

--- LeagueStarter/Pages/Teams/Index.cshtml.cs
+++ LeagueStarter/Pages/Teams/Index.cshtml.cs
@@ -2,15 +2,48 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using League.Data;
+using League.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
 
 namespace League.Pages.Teams
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        // Injecting the EF context
+        private readonly LeagueContext _context;
+
+        public IndexModel(LeagueContext context)
         {
+            _context = context;
+        }
+
+        // Load all teams, conferences and divisions
+        public List<Conference> Conferences { get; set; }
+        public List<Division> Divisions { get; set; }
+        public List<Team> Teams { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Conferences = await _context.Conferences.ToListAsync();
+            Divisions = await _context.Divisions.ToListAsync();
+            Teams = await _context.Teams.ToListAsync();
+        }
+
+        // Get all Divisions from a Conference and sort them by Name
+        public List<Division> GetConferenceDivisions(string ConferenceId)
+        {
+            return Divisions.Where(d => d.ConferenceId.Equals(ConferenceId)).OrderBy(d => d.Name).ToList();
+        }
+
+        // Get all Teams from a Division and sort them by Win
+        public List<Team> GetDivisonTeams(string DivisionId)
+        {
+            return Teams.Where(t => t.DivisionId.Equals(DivisionId)).OrderByDescending(t => t.Win).ToList();
         }
     }
 }
