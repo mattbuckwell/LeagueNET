cb3c296d7bf0985a00391318ce489977034f05b4
Matt B <mattbuckwell@gmail.com>
2022-09-15 16:11:40 +1200

    Code and markup to display selected players details.

--- LeagueStarter/Pages/Index.cshtml.cs
+++ LeagueStarter/Pages/Index.cshtml.cs
@@ -2,24 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using League.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using League.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace League.Pages
 {
   public class IndexModel : PageModel
   {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly LeagueContext _context;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(LeagueContext context)
     {
-      _logger = logger;
+            _context = context;
     }
 
-    public void OnGet()
-    {
+    public League.Models.League League { get; set; }
 
+    public async Task OnGetAsync()
+    {
+       League = await _context.Leagues.FirstOrDefaultAsync();
     }
-  }
+   }
 }
